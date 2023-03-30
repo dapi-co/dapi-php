294c4a33fbff21bb34dbb20abfc8935ed9727d08
vinayak-dapi <vinayak@dapi.co>
2023-03-30 14:05:46 +0530

    CS-2242: Add headers for slack noti

--- src/Products/DapiRequester.php
+++ src/Products/DapiRequester.php
@@ -9,6 +9,8 @@ class DapiRequester
   private $API_BASE_URL;
   private $DD_HOST;
   private const USER_AGENT = 'Dapi Connect PHP';
+  private const LibraryPlatform = "dapi-php";
+  private const LibraryVersion = "1.3.0";
 
   function __construct($appSecret)
   {
@@ -38,7 +40,8 @@ class DapiRequester
 
     $headers['User-Agent'] = self::USER_AGENT;
     $headers['Content-Type'] = 'application/json';
-
+    $headers['X-Dapi-Library'] = self::LibraryPlatform;
+    $headers['X-Dapi-Library-Version'] = self::LibraryVersion;
     $url = '';
     if ($comingFromSdk) {
       $url = $this->DD_HOST;
